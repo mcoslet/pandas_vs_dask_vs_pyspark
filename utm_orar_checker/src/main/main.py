c4b10c82f5ecad43a1a0dd845dceb9942447bcc3
mcoslet <mcoslet@griddynamics.com>
2024-02-07 17:41:54 +0200

    First commit

--- utm_orar_checker/src/main/main.py
+++
@@ -1,8 +0,0 @@
-from services.scraper.parse_page import Parser
-from services.utils.constants import FcimUrls
-from services.utils.files import check_if_schedule_is_updated
-
-if __name__ == '__main__':
-    parser = Parser(FcimUrls.SCHEDULE_URL)
-    first_year, second_year = parser.parse_schedule_link()
-    check_if_schedule_is_updated(first_year)
